370df564a73f708eb9786b413c625cb2e8769af9
Barış Baran Çiçek <barisbarancicek@hotmail.com>
2021-01-16 15:48:43 +0300

    Users name and date displayed.
    Logged in users full name and current date are displayed in every page.

--- navbarManager.php
+++ navbarManager.php
@@ -1,20 +1,29 @@
-<?php
-//Navbar to go between pages for manager.
-?>   
-   <div class="navbar">
-       <li class="dropdown" style="display:inline-block;"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Residents <span class="caret"></span></a>
-          <ul class="dropdown-menu" style="background-color:#333;">
-            <li><div class="navbar" style="margin:0px;"><a style="width:130px;" href="ResidentListPage(Manager).php">Resident List</a></div></li>
-            <li><div class="navbar" style="margin:0px;"><a style="width:130px;" href="UserRegistrationPage.php">Register User</a></div></li>
-            <li><div class="navbar" style="margin:0px;"><a style="width:160px;" href="ResidentRegistrationPage.php">Register Resident</a></div></li>
-          </ul>
-       </li>
-       <li style="display:inline;"><a href="AccountPage(Manager).php">Account</a></li>
-       <li style="display:inline;"><a href="AnnouncementPage(Manager).php">Announcement</a></li>
-       <li style="display:inline;"><a href="InformationPage(Manager).php">Information</a></li>
-       <li style="display:inline;"><a href="IncomeExpensesPage(Manager).php">Incomes/Expenses</a></li>
-       <a href="DuesPage.php">Dues</a>
-       <li style="display:inline;"><a id="logOut" href="LogOut.php">Log Out</a></li>
-    </div>
-<?php
-?>
+<!-- Current users name and current date. -->
+<div class="navbar" style="width:20%; height:70px; color:white; text-align:center; font-size:19px;">
+   <?php
+   $current_fullname = $_SESSION["c_fullname"];
+   echo "$current_fullname";
+   ?> 
+<br>
+   <?php
+   $currentDate= date("d-m-Y");
+   echo "$currentDate";
+   ?> 
+</div>
+
+<!-- Navbar to go between pages for manager. -->
+<div class="navbar">
+   <li class="dropdown" style="display:inline-block;"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Residents <span class="caret"></span></a>
+      <ul class="dropdown-menu" style="background-color:#333;">
+         <li><div class="navbar" style="margin:0px;"><a style="width:130px;" href="ResidentListPage(Manager).php">Resident List</a></div></li>
+         <li><div class="navbar" style="margin:0px;"><a style="width:130px;" href="UserRegistrationPage.php">Register User</a></div></li>
+         <li><div class="navbar" style="margin:0px;"><a style="width:160px;" href="ResidentRegistrationPage.php">Register Resident</a></div></li>
+      </ul>
+      </li>
+      <li style="display:inline;"><a href="AccountPage(Manager).php">Account</a></li>
+      <li style="display:inline;"><a href="AnnouncementPage(Manager).php">Announcement</a></li>
+      <li style="display:inline;"><a href="InformationPage(Manager).php">Information</a></li>
+      <li style="display:inline;"><a href="IncomeExpensesPage(Manager).php">Incomes/Expenses</a></li>
+      <a href="DuesPage.php">Dues</a>
+      <li style="display:inline;"><a id="logOut" href="LogOut.php">Log Out</a></li>
+</div>
--- navbarResident.php
+++ navbarResident.php
@@ -1,13 +1,22 @@
-<?php
-//Navbar to go between pages for resident.
-?>
-<div class="navbar">
-        <a href="AccountPage(Resident).php">Account</a>
-        <a href="AnnouncementPage(Resident).php">Announcement</a>
-        <a href="InformationPage(Resident).php">Information</a>
-        <a href="IncomeExpensesPage(Resident).php">Incomes/Expenses</a>
-        <a href="DuesPage.php">Dues</a>
-        <a id="logOut" href="LogOut.php">Log Out</a>
+<!-- Current users name and current date. -->
+<div class="navbar" style="width:20%; height:70px; color:white; text-align:center; font-size:19px;">
+   <?php
+   $current_fullname = $_SESSION["c_fullname"];
+   echo "$current_fullname";
+   ?> 
+<br>
+   <?php
+   $currentDate= date("d-m-Y");
+   echo "$currentDate";
+   ?> 
 </div>
-<?php
-?>
+
+<!-- Navbar to go between pages for resident. -->
+<div class="navbar">
+  <a href="AccountPage(Resident).php">Account</a>
+  <a href="AnnouncementPage(Resident).php">Announcement</a>
+  <a href="InformationPage(Resident).php">Information</a>
+  <a href="IncomeExpensesPage(Resident).php">Incomes/Expenses</a>
+  <a href="DuesPage.php">Dues</a>
+  <a id="logOut" href="LogOut.php">Log Out</a>
+</div>
